fc8fa1185b492ae21a5a721e52470c1a282185fa
t-amqi <umberos@gmail.com>
2017-08-24 15:22:24 -0700

    It works!

--- src/extension.ts
+++ src/extension.ts
@@ -6,12 +6,16 @@
 import * as fs from 'fs';
 import * as path from 'path';
 import * as vscode from 'vscode';
+import {
+    IPCMessageReader, IPCMessageWriter,
+    createConnection, IConnection,
+    InitializeParams, CancellationToken,
+    InitializeResult, DiagnosticSeverity
+} from 'vscode-languageserver';
 
 export function activate(context: vscode.ExtensionContext) {
-
+    let counter = 0;
     const regexRegex = /^(\s*)("regexp":) "(.+)",$/g;
-    //const aaaaRegexRegex = /^(\s*)("pattern":)(\s+)\[((\s*{\s*)"regexp":(.*)(,)?\n([^}]*)}(,)?)+/g;
-    const dumbRegex = /.*/g;
     const multiRegexRegex = /^(\s*)"pattern":\s*\[/g;
     const regexHighlight = vscode.window.createTextEditorDecorationType({ backgroundColor: 'rgba(100,100,100,.35)' });
     const matchHighlight = vscode.window.createTextEditorDecorationType({ backgroundColor: 'rgba(255,255,0,.35)' });
@@ -22,6 +26,10 @@ export function activate(context: vscode.ExtensionContext) {
     const languages = ['json'];
 
     const decorators = new Map<vscode.TextEditor, RegexMatchDecorator>();
+
+    const editor = vscode.window.activeTextEditor;
+
+    let problemsCollection: any[] = [];
 
     context.subscriptions.push(vscode.commands.registerCommand('extension.toggleRegexPreview', toggleRegexPreview));
 
@@ -34,8 +42,19 @@ export function activate(context: vscode.ExtensionContext) {
     const interval = setInterval(() => updateDecorators(), 5000);
     context.subscriptions.push({ dispose: () => clearInterval(interval) });
 
+    let source = `Regex`;
+    let diagnosticCollection: vscode.DiagnosticCollection = vscode.languages.createDiagnosticCollection(source);
+    if (editor) {
+        let basicUri = editor.document.uri;
+        let diagnosticCollections = [];
+        diagnosticCollections.push(diagnosticCollection);
+        let basicPath = basicUri.path;
+        basicPath = basicPath.slice(0, basicPath.lastIndexOf('/'));
+    }
+
     function provideCodeLenses(document: vscode.TextDocument, token: vscode.CancellationToken) {
-        const matches = findRegexes(document);
+        counter = 0;
+        const matches = findRegexes(document).concat(findMultiRegexes(document, problemsCollection));
         return matches.map(match => new vscode.CodeLens(match.range, {
             title: 'Test Regex...',
             command: 'extension.toggleRegexPreview',
@@ -165,6 +184,10 @@ export function activate(context: vscode.ExtensionContext) {
 
         range: vscode.Range;
 
+        group?: RegExp[];
+
+        index?: number;
+
     }
 
     interface Match {
@@ -270,6 +293,7 @@ export function activate(context: vscode.ExtensionContext) {
     }
 
     function findRegexes(document: vscode.TextDocument) {
+        problemsCollection = [];
         const matches: RegexMatch[] = [];
         for (let i = 0; i < document.lineCount; i++) {
             const line = document.lineAt(i);
@@ -287,6 +311,17 @@ export function activate(context: vscode.ExtensionContext) {
                 }
             }
         }
+
+        return matches
+    }
+
+    function findMultiRegexes(document: vscode.TextDocument, problemsCollection: any[]) {
+        const matches: RegexMatch[] = [];
+        let documentText = JSON.parse(document.getText());
+        let problems = null;
+        if (documentText['contributes']) {
+            problems = documentText['contributes']['problemMatchers'];
+        }
         // for multiline pms
         let locationOfRegex = [];
         for (let i = 0; i < document.lineCount; i++) {
@@ -299,37 +334,54 @@ export function activate(context: vscode.ExtensionContext) {
                 locationOfRegex.push(i);
             }
         }
-        
-        let documentText = JSON.parse(document.getText());
-        let problems = null;
-        if(documentText['contributes']){
-            problems = documentText['contributes']['problemMatchers'];
-        }
-        let counter = 0;
-        if (problems){
-            for(let i = 0; i < problems.length; i++){
-                if(problems[i]['pattern']){
+
+        if (problems) {
+            for (let i = 0; i < problems.length; i++) {
+                const mult = problems[i]['pattern'];
+                let line = [];
+                if (mult) {
                     let multiReg = [];
-                    for (let j = 0; j < problems[i]['pattern'].length; ++j){
-                        if(problems[i]['pattern'][j]['regexp']){
-                            multiReg.push(problems[i]['pattern'][j]['regexp']);
+                    // get all the regexes for the multiline problem matcher
+                    for (let j = 0; j < mult.length; ++j) {
+                        if (mult[j]['regexp']) {
+                            multiReg.push(mult[j]['regexp']);
                         }
                     }
                     if (multiReg.length > 0) {
-                        if(problems[i]['pattern'][problems[i]['pattern'].length-1]['loop'] === true) {
-                            multiReg[multiReg.length - 1] = '('+multiReg[multiReg.length - 1]+'\\n?)+';
+                        if (mult[mult.length - 1]['loop'] === true) {
+                            // if the loop property is true, we allow one or more instances of the last regex
+                            multiReg[multiReg.length - 1] = '(' + multiReg[multiReg.length - 1] + '\\n?)+';
                         }
-                        const out = RegExp(multiReg.join('\\n'));
-                        const result ={document:document, regex:out, range:new vscode.Range(locationOfRegex[counter], 0 ,locationOfRegex[counter], document.lineAt(locationOfRegex[counter]).text.length)};
-                        if(result){
+                        const out = multiReg.map(element => RegExp(element));
+                        const result = { index: counter, document: document, regex: RegExp(multiReg.join('\\n')), range: new vscode.Range(locationOfRegex[counter], 0, locationOfRegex[counter], document.lineAt(locationOfRegex[counter]).text.length), group: out };
+                        if (result) {
                             matches.push(result);
+                            line.push(extractGroups(mult));
                         }
                     }
                 }
+                problemsCollection.push(line);
                 counter++
             }
         }
         return matches;
+    }
+
+    function extractGroups(problemMatcherInstance: any[]) {
+        const validProperties = ['line', 'column', 'severity', 'code', 'file', 'location', 'endColumn', 'endLine', 'message'];
+        let data: any = {};
+        let counterOfGroups = 0;
+        let diagnosticInfo = {};
+        for (let j = 0; j < problemMatcherInstance.length; ++j) {
+            let tmp: any = {};
+            for (let c in validProperties) {
+                if (problemMatcherInstance[j][validProperties[c]] && (!data[j] || !data[j][validProperties[c]])) {
+                    tmp[validProperties[c]] = problemMatcherInstance[j][validProperties[c]]
+                }
+            }
+            data[j] = tmp
+        }
+        return data;
     }
 
     function getRegexRegex(flag: boolean) {
@@ -343,7 +395,7 @@ export function activate(context: vscode.ExtensionContext) {
             return {
                 document: document,
                 regex: regex,
-                range: new vscode.Range(line, match.index, line, match.index + match[0].length + match[1].length  + match[index].length)
+                range: new vscode.Range(line, match.index, line, match.index + match[0].length + match[1].length + match[index].length)
             };
         }
     }
@@ -361,15 +413,147 @@ export function activate(context: vscode.ExtensionContext) {
         const matches: Match[] = [];
         const regex = addGM(regexMatch.regex);
         let match: RegExpExecArray | null;
+        let toAdd: vscode.Diagnostic[] = [];
+        let mapping = '';
+        if (!regexMatch.index) {
+            regexMatch.index = 0;
+        }
         while ((regex.global || !matches.length) && (match = regex.exec(text))) {
+            let message: string | null = null;
+            let file: string | null = null;
+            let range: vscode.Range | null = null;
+            if (!mapping) {
+                if (!regexMatch.group) {
+                    regexMatch.group = [regexMatch.regex];
+                }
+                mapping = computeGroupNumbers(match[0], regexMatch.group, regexMatch.index);
+            }
             matches.push({
                 range: new vscode.Range(document.positionAt(match.index), document.positionAt(match.index + match[0].length))
             });
+            let a: vscode.Diagnostic[] = [];
+
+            if (regexMatch.group) {
+                a = mapGroups(match, regexMatch, regexMatch.index, mapping);
+            } else {
+                regexMatch.group = [regexMatch.regex]
+                const mapping = computeGroupNumbers(match[0], regexMatch.group, regexMatch.index);
+                a = mapGroups(match, regexMatch, regexMatch.index, mapping);
+            }
+
+            for (let i = 0; i < a.length; ++i) {
+                toAdd.push(a[i]);
+            }
+
             // Handle empty matches (fixes #4)
             if (regex.lastIndex === match.index) {
                 regex.lastIndex++;
             }
         }
+        let editor = vscode.window.activeTextEditor;
+        if (editor) {
+            // send to problems
+            diagnosticCollection.set(editor.document.uri, toAdd);
+        }
         return matches;
+    }
+
+    function mapGroups(match: RegExpExecArray, regexes: RegexMatch, counter: number, mapping: any): vscode.Diagnostic[] {
+        let matchLines = match;
+        const regexLines = regexes.group;
+        let pro: vscode.Diagnostic[] = [];
+        if (regexLines) {
+            let mapped: vscode.Diagnostic;
+            problemsCollection;
+            let range: vscode.Range = new vscode.Range(0, 0, 0, 0);
+            let message: string = '';
+            let source: string = '';
+            let code: string | number = '';
+            let flag: boolean = false;
+            let severity: vscode.DiagnosticSeverity = vscode.DiagnosticSeverity.Error;
+            try {
+                code = matchLines[mapping['code']];
+                source = matchLines[mapping['file']];
+                message = matchLines[mapping['message']];
+                if (mapping['severity'] !== -1 && matchLines.length > mapping['severity']) {
+                    if (matchLines[mapping['severity']].toLowerCase() === 'warning') {
+                        severity = vscode.DiagnosticSeverity.Warning;
+                    }
+                    if (matchLines[mapping['severity']].toLowerCase() === 'error') {
+                        severity = vscode.DiagnosticSeverity.Error;
+                    }
+                    if (matchLines[mapping['severity']].toLowerCase() === 'info') {
+                        severity = vscode.DiagnosticSeverity.Information;
+                    }
+                    if (matchLines[mapping['severity']].toLowerCase() === 'hint') {
+                        severity = vscode.DiagnosticSeverity.Hint;
+                    }
+                }
+                if (mapping['location'] > -1 && matchLines.length > mapping['location']) {
+                    let a = matchLines[mapping['location']].split(',');
+                    let b = a.map(element => Number.parseInt(element));
+                    if (a.length === 1) {
+                        range = new vscode.Range(b[0], 0, b[0], 0);
+                        flag = true;
+                    } else if (a.length === 2) {
+                        range = new vscode.Range(b[0], b[1], b[0], b[1]);
+                        flag = true;
+                    } else if (a.length === 4) {
+                        range = new vscode.Range(b[0], b[1], b[3], b[4]);
+                        flag = true;
+                    }
+                } else {
+                    // make a range from columns
+                    let endl = matchLines[mapping['endline']];
+                    let endc = matchLines[mapping['endcolumn']];
+                    if (!endl) {
+                        endl = matchLines[mapping['line']];
+                    }
+                    if (!endc) {
+                        endc = matchLines[mapping['column']]
+                    }
+                    range = new vscode.Range(Number.parseInt(matchLines[mapping['line']]), Number.parseInt(matchLines[mapping['column']]),
+                        Number.parseInt(endl), Number.parseInt(endc));
+                }
+            } catch (ex) {
+                // console.log(ex);
+            }
+            if (message && source && flag) {
+                if (!code) {
+                    code = 'a' + Math.random().toString();
+                }
+                pro.push({ source: source, range: range, message: message, code: code, severity: severity });
+            }
+        }
+
+        return pro;
+    }
+
+    function computeGroupNumbers(match: string, regexes: RegExp[], aaaa: number) {
+        const validProperties = ['line', 'column', 'severity', 'code', 'file', 'location', 'endColumn', 'endLine', 'message'];
+        let counter = 0;
+        let flag = false;
+        let a: any = { 'line': 1, 'column': 3, 'severity': -1, 'code': -1, 'file': 1, 'location': -1, 'endColumn': -1, 'endLine': -1, 'message': 4 };
+        for (let i = 0; i < regexes.length; ++i) {
+            let h = problemsCollection[aaaa][0];
+            for (let j = 0; j < validProperties.length; ++j) {
+                if (h[i][validProperties[j]]) {
+                    if (validProperties[j] === 'location' && !flag) {
+                        a['message'] = 5;
+                    }
+                    if (validProperties[j] === 'message') {
+                        flag = true;
+                    }
+                    a[validProperties[j]] = h[i][validProperties[j]] + counter;
+                }
+            }
+            regexes[i] = RegExp(regexes[i].source.replace(/^\^/, ''));
+            regexes[i] = RegExp(regexes[i].source.replace(/\$$/, ''));
+            let groupings = regexes[i].exec(match);
+            if (groupings) {
+                counter += groupings.length - 1;
+            }
+        }
+        return a;
     }
 }
